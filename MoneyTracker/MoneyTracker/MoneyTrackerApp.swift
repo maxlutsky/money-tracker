6abd886db8d5579ee0e88e1cf65bb8b4766a1879
Max Lutskyi <maxlutsky@gmail.com>
2026-03-17 08:31:45 +0100

    create empty project

---
+++ MoneyTracker/MoneyTracker/MoneyTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyTrackerApp.swift
+//  MoneyTracker
+//
+//  Created by Max Lutsky on 17.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct MoneyTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MoneyTracker/MoneyTrackerTests/MoneyTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyTrackerTests.swift
+//  MoneyTrackerTests
+//
+//  Created by Max Lutsky on 17.03.2026.
+//
+
+import Testing
+@testable import MoneyTracker
+
+struct MoneyTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
